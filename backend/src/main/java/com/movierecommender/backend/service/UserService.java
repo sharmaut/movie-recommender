673655586a05673731743de64eb76ae0cbe77931
sharmaut <utsavsh091@gmail.com>
2026-03-31 00:17:21 +1100

    feat: add SecurityConfig and BCrypt password hashing

--- backend/src/main/java/com/movierecommender/backend/service/UserService.java
+++ backend/src/main/java/com/movierecommender/backend/service/UserService.java
@@ -2,15 +2,18 @@ package com.movierecommender.backend.service;
 
 import com.movierecommender.backend.entity.User;
 import com.movierecommender.backend.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User registerUser(String name, String email, String password) {
@@ -21,13 +24,14 @@ public class UserService {
         User user = new User();
         user.setName(name);
         user.setEmail(email);
-        user.setPassword(password);
+        user.setPassword(passwordEncoder.encode(password));
 
         return userRepository.save(user);
     }
 
-        public User findByEmail(String email) {
-            return userRepository.findByEmail((email))
-                    .orElseThrow(() -> new RuntimeException("User not found"));
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
     }
+
 }
